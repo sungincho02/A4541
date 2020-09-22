098ab630dc3c3200d9a37e9de03c6eff606e5530
sungincho02 <71339664+sungincho02@users.noreply.github.com>
2020-09-22 12:40:28 -0700

    Update Palindrome.cpp

--- Palindrome.cpp
+++ Palindrome.cpp
@@ -11,22 +11,31 @@ int main()
   cout << "Enter a text" << endl;
 
   cin.get(input, 80);
-  cin.get();
+
+  cout << input << endl;
 
   int count = 0;
   for (int i = 0; i < 80; i++) {
-    if (isalnum(input[i])) {
-      alnum[count] = input[i];
-      count++;
+    if (input[i] != '\0') {
+      if (isalnum(input[i])) {
+        alnum[count] = input[i];
+        count++;
+      }
+    }
+    else {
+      i = 80;
     }
   }
   alnum[count] = '\0';
+  cout << count << endl;
+  cout << alnum << endl;
 
   char reverse[count];
   for (int i = 0; i < count; i++) {
     reverse[i] = alnum[count - 1 - i];
   }
   reverse[count] = '\0';
+  cout << reverse << endl;
 
   if (strcmp(alnum, reverse) == 0) {
     cout << "Palindrome" << endl;
