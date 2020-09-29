631fc22b32c6d0c6d7543bf05400d524cc7aa682
sungincho02 <71339664+sungincho02@users.noreply.github.com>
2020-09-29 09:23:39 -0700

    Update TicTacToe.cpp

--- TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include <vector>
 
+using namespace std;
+
+int checkWin();
+
+int main() {
+  const bool XT = true,
+    OT = false;
+  const char N = ' ',
+    X = 'X',
+    O = 'O';
+  bool turn = XT;
+  vector<vector<char>> board{{N, N, N},
+                             {N, N, N},
+                             {N, N, N}};
+
+
+
+  return 0;
+}
+
+int checkWin() {
+
+}
