50981b3fc92f52e0dddd9cca7bc3afea8122ee22
sungincho02 <sungincho02@gmail.com>
2020-10-12 05:00:06 +0100

    Update StudentList.cpp

--- StudentList/StudentList.cpp
+++ StudentList/StudentList.cpp
@@ -13,28 +13,31 @@ struct Student {
   float gpa;
 };
 
-vector<Student> list;
-
-void add();
-void print();
-void del();
+void add(vector<Student*>* list);
+void print(vector<Student*>* list);
+void del(vector<Student*>* list);
 
 int main() {
+  vector<Student*>* list = new vector<Student*>();
   bool quit = false;
   string input;
+
+  cout.setf(ios::fixed, ios::floatfield);
+  cout.setf(ios::showpoint);
+  cout.precision(2);
   
   while (!quit) {
     cout << "\nEnter a command" << endl;
     cin >> input;
 
     if (input == "ADD") {
-      add();
+      add(list);
     }
     else if (input == "PRINT") {
-      print();
+      print(list);
     }
     else if (input == "DELETE") {
-      del();
+      del(list);
     }
     else if (input == "QUIT") {
       quit = true;
@@ -47,49 +50,58 @@ int main() {
   return 0;
 }
 
-void add() {
-  Student nstdnt;
+void add(vector<Student*>* list) {
+  Student* nstdnt = new Student();
   cout << "\nFirst name:" << endl;
-  cin >> nstdnt.fn;
+  cin >> nstdnt -> fn;
   cin.ignore(numeric_limits<streamsize>::max(), '\n');
   cout << "Last name:" << endl;
-  cin >> nstdnt.ln;
+  cin >> nstdnt -> ln;
   cin.ignore(numeric_limits<streamsize>::max(), '\n');
   cout << "Student ID:" << endl;
-  cin >> nstdnt.id;
-  while (nstdnt.id < 0) {
+  cin >> nstdnt -> id;
+  while (nstdnt ->id < 0) {
     cout << "Invalid student ID" << endl;
-    cin >> nstdnt.id;
+    cin >> nstdnt -> id;
   }
   cin.ignore(numeric_limits<streamsize>::max(), '\n');
   cout << "GPA:" << endl;
-  cin >> nstdnt.gpa;
-  while (nstdnt.gpa > 5 || nstdnt.gpa < 0) {
+  cin >> nstdnt -> gpa;
+  while (nstdnt -> gpa > 5 || nstdnt -> gpa < 0) {
     cout << "Invalid GPA" << endl;
-    cin >> nstdnt.gpa;
+    cin >> nstdnt -> gpa;
   }
-
-  list.push_back(nstdnt);
+  cin.ignore(numeric_limits<streamsize>::max(), '\n');
+  
+  list -> push_back(nstdnt);
   cout << "Student added" << endl;
 }
 
-void print() {
+void print(vector<Student*>* list) {
   cout << endl;
-  for (int i = 0; i < list.size(); i++) {
-    cout << list[i].fn << " " << list[i].ln << ", " << list[i].id << ", " << list[i].gpa << endl;
+  for (int i = 0; i < list -> size(); i++) {
+    cout << list -> at(i) -> fn << " " << list -> at(i) -> ln << ", " << list -> at(i) -> id << ", " << list -> at(i) -> gpa << endl;
   }
 }
 
-void del() {
+void del(vector<Student*>* list) {
   int id;
+  bool valid = false;
   
   cout << "\nEnter the student ID" << endl;
   cin >> id;
 
-  for (int i = 0; i < list.size(); i++) {
-    if (id == list[i].id) {
-      list.erase(list.begin() + i);
+  for (int i = 0; i < list -> size(); i++) {
+    if (id == list -> at(i) -> id) {
+      list -> erase(list -> begin() + i);
+      valid = true;
     }
   }
-  cout << "Student deleted" << endl;
+
+  if (valid) {
+    cout << "Student deleted" << endl;
+  }
+  else {
+    cout << "That student does not exist" << endl;
+  }
 }
