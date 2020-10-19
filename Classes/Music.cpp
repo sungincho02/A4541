81b01fe051d4ee26fc649a0638b964ce95630196
sungincho02 <sungincho02@gmail.com>
2020-10-19 05:01:23 +0100

    Update Music

--- Classes/Music.cpp
+++ Classes/Music.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+#include <string.j>
+#include "Music.h"
 
+using namespace std;
+
+Music::Music(string* ntitle, int nyear, string* nartist, int nduration, string* npublisher) {
+  setTitle(ntitle);
+  setYear(nyear);
+  artist = nartist;
+  duration = nduration;
+  publisher = npublisher;
+}
+
+string* Music::getArtist() {
+  return artist;
+}
+
+int Music::getDuration() {
+  return duration;
+}
+
+string* Music::getPublisher() {
+  return publisher;
+}
--- Classes/Music.h
+++ Classes/Music.h
@@ -1,1 +1,17 @@
+#include <iostream>
+#include <string.h>
+#include "Media.h"
 
+using namespace std;
+
+class Music: public Media {
+ public:
+  Music(string* ntitle, int nyear, string* artist, int nduration, string* npublisher);
+  string* getArtist();
+  int getDuration();
+  string* getPublisher();
+ private:
+  string* artist;
+  int duaration;
+  string* publisher;
+};
