f5ea7a15f9b9bf4538a77d4b968989ddef65f655
sungincho02 <71339664+sungincho02@users.noreply.github.com>
2020-10-19 02:16:27 -0700

    Update Music.h

--- Classes/Music.h
+++ Classes/Music.h
@@ -1,3 +1,4 @@
+// declaration file for Music class
 #include <iostream>
 #include <string.h>
 #include "Media.h"
@@ -6,8 +7,11 @@ using namespace std;
 
 class Music: public Media {
  public:
+  // constructor
   Music(string* ntitle, int nyear, string* artist, int nduration, string* npublisher);
+  // destructor
   ~Music();
+  // accessors
   string* getArtist();
   int getDuration();
   string* getPublisher();
