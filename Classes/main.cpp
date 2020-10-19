22000b98b468ea98dd7cbd20c53dd9d76df7cfba
sungincho02 <71339664+sungincho02@users.noreply.github.com>
2020-10-19 02:35:35 -0700

    Update main.cpp

--- Classes/main.cpp
+++ Classes/main.cpp
@@ -1,3 +1,10 @@
+/*
+ * Project: Classes
+ * Author: Sungin Cho
+ * Date: 10/19/20
+ * Instructor: Jason Galbraith
+ * Description: This is a program for managing a list of digital medias
+ */
 #include <iostream>
 #include <string>
 #include <vector>
@@ -10,10 +17,12 @@
 
 using namespace std;
 
+// global variables
 vector<Media*> list;
 string dtitle;
 int dyear;
 
+//function prototypes
 void addVG();
 void addMusic();
 void addMovie();
@@ -27,15 +36,18 @@ int main() {
   bool quit = false;
   string input;
 
+  // setting precision for ratings
   cout.setf(ios::fixed, ios::floatfield);
   cout.setf(ios::showpoint);
   cout.precision(1);
   
   while (!quit) {
+    // prompt for a user command and resolve
     cout << "\nEnter a command" << endl;
     cin >> input;
 
     if (input == "ADD") {
+      // prompt for the type of digital media
       cout << "\nChoose a digital media you want to add:\n1) Video Game\n2) Music\n3) Movie" << endl;
       cin >> input;
 
@@ -69,12 +81,14 @@ int main() {
   return 0;
 }
 
+// Add a new Videogame to the list
 void addVG() {
   string title;
   int year;
   string publisher;
   float rating;
   
+  // prompt for the video game details
   cout << "\nTitle:" << endl;
   cin.ignore(numeric_limits<streamsize>::max(), '\n');
   getline(cin, title);
@@ -89,11 +103,13 @@ void addVG() {
   cout << "Rating;" << endl;
   cin >> rating;
 
+  // create new Videogame and add to the list
   Media* nmedia = new Videogame(new string(title), year, new string(publisher), rating);
   list.push_back(nmedia);
   cout << "Video game added" << endl;
 }
 
+// Add a new Music to the list
 void addMusic() {
   string title;
   int year;
@@ -101,6 +117,7 @@ void addMusic() {
   int duration;
   string publisher;
   
+  // prompt for the music details
   cout << "\nTitle:" << endl;
   cin.ignore(numeric_limits<streamsize>::max(), '\n');
   getline(cin, title);
@@ -119,11 +136,13 @@ void addMusic() {
   cin.ignore(numeric_limits<streamsize>::max(), '\n');
   getline(cin, publisher);
 
+  // create new Music and add to the list
   Media* nmedia = new Music(new string(title), year, new string(artist), duration, new string(publisher));
   list.push_back(nmedia);
   cout << "Music added" << endl;
 }
 
+// Add a new Movie to the list
 void addMovie() {
   string title;
   int year;
@@ -131,6 +150,7 @@ void addMovie() {
   int duration;
   float rating;
   
+  // prompt for the movie details
   cout << "\nTitle:" << endl;
   cin.ignore(numeric_limits<streamsize>::max(), '\n');
   getline(cin, title);
@@ -148,19 +168,23 @@ void addMovie() {
   cout << "Rating;" << endl;
   cin >> rating;
 
+  // create a new Movie and add to the list
   Media* nmedia = new Movie(new string(title), year, new string(director), duration, rating);
   list.push_back(nmedia);
   cout << "Movie added" << endl;
 }
 
+// search for digital media in the list with title or year
 void search() {
   string input;
   bool nf = true;
   
+  // prompt for search type
   cout << "\nSearch with:\n1) Title\n2) Year" << endl;
   cin >> input;
   
   if (input == "1") {
+    // print all digital media with matching title
     cout << "\nEnter the title:" << endl;
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
     getline(cin, input);
@@ -171,11 +195,14 @@ void search() {
 	nf = false;
       }
     }
+	  
+    // notify user if none of them matched
     if (nf) {
       cout << "There's no digital media with that title" << endl;
     }
   }
   else if (input == "2") {
+    // print all digital media with matching year
     cout << "\nEnter the year:" << endl;
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
     getline(cin, input);
@@ -186,6 +213,8 @@ void search() {
 	nf = false;
       }
     }
+	  
+    // notify user if none of them matched
     if (nf) {
       cout << "There's no digital media from that year" << endl;
     }
@@ -195,7 +224,9 @@ void search() {
   }
 }
 
+// print out details of given digital media
 void print(Media* media) {
+  // check for the type and print its details
   if (typeid(*media) == typeid(Videogame)) {
     Videogame* ptr = (Videogame*)media;
     cout << "\nType: Video Game" << "\nTitle: " << *(ptr->getTitle()) << "\nYear: " << ptr->getYear() << "\nPublisher: " << *(ptr->getPublisher()) << "\nRating: " << ptr->getRating() << endl;
@@ -210,14 +241,17 @@ void print(Media* media) {
   }
 }
 
+// delete a digital media
 void del() {
   string input;
   bool nf = true;
   
+  // search for digital media to delete
   cout << "\nSearch with:\n1) Title\n2) Year" << endl;
   cin >> input;
   
   if (input == "1") {
+    // search and print result
     cout << "\nEnter the title:" << endl;
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
     getline(cin, input);
@@ -233,6 +267,7 @@ void del() {
       cout << "There's no digital media with that title" << endl;
     }
     else {
+      // ask for confirmation
       cout << "\nDelete these items? [Y/N]" << endl;
       cin >> input;
 
@@ -240,16 +275,20 @@ void del() {
 	cout << "Please answer with a \"Y\" or \"N\"" << endl;
 	cin >> input;
       }
+	    
       if (input == "Y") {
+        // delete all digital media with matching title 
 	list.erase(remove_if(list.begin(), list.end(), titleMatch), list.end());
 	cout << "Items deleted" << endl;
       }
       else {
+	// terminate deletion
 	cout << "Deletion terminated" << endl;
       }
     }
   }
   else if (input == "2") {
+    // search and print result
     cout << "\nEnter the year:" << endl;
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
     getline(cin, input);
@@ -266,6 +305,7 @@ void del() {
       cout << "There's no digital media from that year" << endl;
     }
     else {
+      // ask for confirmation
       cout << "\nDelete these items? [Y/N]" << endl;
       cin >> input;
       
@@ -274,10 +314,12 @@ void del() {
 	cin >> input;
       }
       if (input == "Y") {
+	// delete all digital media with matching year
 	list.erase(remove_if(list.begin(), list.end(), yearMatch), list.end());
 	cout << "Items deleted" << endl;
       }
       else {
+	// terminate deletion
 	cout << "Deletion terminated" << endl;
       }
     }
@@ -287,10 +329,12 @@ void del() {
   }
 }
 
+// check for title match
 bool titleMatch(Media* media) {
   return (*(media->getTitle()) == dtitle);
 }
 
+// check for year match
 bool yearMatch(Media* media) {
   return (media->getYear() == dyear);
 }
