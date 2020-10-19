d1c0efb47cc7a973a5f7050524ad90cf61ff87e2
sungincho02 <sungincho02@gmail.com>
2020-10-19 04:50:56 +0100

    update Video

--- Classes/Video.cpp
+++ Classes/Video.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#include <string.h>
+#include "Video.h"
 
+using namespace std;
+
+Video::Video(string* ntitle, int nyear, string* npublisher, float nrating) {
+  setTitle(ntitle);
+  setYear(nyear);
+  publisher = npublisher;
+  rating = nrating;
+}
+
+string* Video::getPublisher() {
+  return publisher;
+}
+
+float Video::getRating() {
+  return rating;
+}
--- Classes/Video.h
+++ Classes/Video.h
@@ -1,1 +1,15 @@
+#include <iostream>
+#include <string.h>
+#include "Media.h"
 
+using namespace std;
+
+class Video: public Media {
+ public:
+  Video(string* ntitle, int nyear, string* npublisher, flaot nrating);
+  string* getPublisher();
+  float getRating();
+ private:
+  string* publisher;
+  float rating;
+};
