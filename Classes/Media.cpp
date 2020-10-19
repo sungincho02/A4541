144aea76aad1dbea1505b057882258d330357c34
sungincho02 <sungincho02@gmail.com>
2020-10-19 04:26:15 +0100

    Updated Media

--- Classes/Media.cpp
+++ Classes/Media.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
+#include <string.h>
+#include "Media.h"
 
+using namespace std;
+
+string* Media::getTitle() {
+  return title;
+}
+
+int Media::getYear() {
+  return year;
+}
--- Classes/Media.h
+++ Classes/Media.h
@@ -1,1 +1,13 @@
+#include <iostream>
+#include <string.h>
 
+using namespace std;
+
+class Media {
+ public:
+  string* getTitle();
+  int getYear();
+ private:
+  string* title;
+  int year;
+};
