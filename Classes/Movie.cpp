49d5bd543f3493b4465dd397dfb4727e32ba28da
sungincho02 <sungincho02@gmail.com>
2020-10-19 05:07:58 +0100

    Update Movie

--- Classes/Movie.cpp
+++ Classes/Movie.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+#include <string.h>
+#include "Movie.h"
 
+using namespace std;
+
+Movie::Movie(string* ntitle, int nyear, string* ndirector, int nduration, float nrating) {
+  setTitle(ntitle);
+  setYear(nyear);
+  director = ndirector;
+  duration = nduration;
+  rating = nrating;
+}
+
+string* Movie::getDirector() {
+  return director;
+}
+
+int Movie::getDuration() {
+  return duration;
+}
+
+float Movie::getRating() {
+  return rating;
+}
--- Classes/Movie.h
+++ Classes/Movie.h
@@ -1,1 +1,17 @@
+#include <iostream>
+#include <string.h>
+#include "Media.h"
 
+using namespace std;
+
+class Movie: public Media {
+ public:
+  Movie(string* ntitle, int nyear, string* ndirector, int nduration, float nrating);
+  string* getDirector();
+  int getDuration();
+  flaot getRating();
+ private:
+  string* director;
+  int duration;
+  flaot rating;
+};
