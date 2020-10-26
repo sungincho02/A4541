bb050bab4fabcdba6dac99a91704d240733a53d1
sungincho02 <sungincho02@gmail.com>
2020-10-26 06:16:18 +0100

    Update Room

--- Zuul/Room.cpp
+++ Zuul/Room.cpp
@@ -1,1 +1,35 @@
+// implementation file for class Room
+#include <iostream>
+#include <cstring>
+#include <map>
+#include <iterator>
+#include "Room.h"
 
+using namespace std;
+
+Room::Room(char ndescription[]) {
+  strcpy(description, ndescription);
+}
+
+Room::~Room() {};
+
+void Room::setExit(char direction, Room* neighbor) {
+  exits.insert({direction, neighbor});
+}
+
+char* Room::getDescription() {
+  return "You are now in a room " + description + "."; 
+}
+
+char* Room::getExit() {
+  char exitStr[99];
+  strcpy(exitStr, "Exits:");
+  for(auto itr = exits.begin(); itr != exits.end(); ++itr) {
+    strcat(exitStr, "\n" + itr->first);
+  }
+  return exitStr;
+}
+
+Room* Room::getNeighbor(char direction) {
+  return exits[direction];
+}
--- Zuul/Room.h
+++ Zuul/Room.h
@@ -1,1 +1,22 @@
+// header file for class Room
+#include <iostream>
+#include <cstring>
+#include <map>
 
+using namespace std;
+
+class Room {
+ public:
+  // constructor
+  Room(char ndescription[]);
+  // destructor
+  ~Room();
+  // accessors
+  void setExit(char direction, Room* neighbor)
+  char* getDescription();
+  char* getExit();
+  Room* getNeighbor(char direction);
+ private:
+  char description[99];
+  map<char, Room*> exits; 
+};
