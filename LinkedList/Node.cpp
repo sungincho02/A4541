9b9a89309b708c52cc6b82b35610459022c12c04
sungincho02 <sungincho02@gmail.com>
2020-11-02 23:27:45 +0000

    Update

--- LinkedList/Node.cpp
+++ LinkedList/Node.cpp
@@ -1,1 +1,27 @@
+// implementation file for Node class
+#include <iostream>
+#include "Node.h"
 
+using namespace std;
+
+Node::Node(Student* nstudent) {
+  student = nstudent;
+  next = NULL;
+}
+
+Node::~Node() {
+  delete student;
+  next = NULL;
+}
+
+Student* Node::getStudent() {
+  return student;
+}
+
+void Node::setNext(Node* node) {
+  next = node;
+}
+
+Node* Node::getNext() {
+  return next;
+}
--- LinkedList/Node.h
+++ LinkedList/Node.h
@@ -1,1 +1,27 @@
+// header file for Node class
+#ifndef NODE_H
+#define NODE_H
 
+#include <iostream>
+#include "Student.h"
+
+using namespace std;
+
+class Node {
+ public:
+  // constructor
+  Node(Student* nstudent);
+  // destructor
+  ~Node();
+  // get student pointer
+  Student* getStudent();
+  // set next node pointer
+  void setNext(Node* node);
+  // get next node pointer
+  Node* getNext();
+ private:
+  Student* student;
+  Node* next;
+};
+
+#endif
