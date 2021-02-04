1383390f4b5e0d35a1869335625c9b14e7484652
sungincho02 <sungincho02@gmail.com>
2021-02-04 03:37:35 +0000

    Update Heap

--- Heap/main.cpp
+++ Heap/main.cpp
@@ -1,6 +1,13 @@
+/*
+ * Project: Heap
+ * Author: Sungin Cho
+ * Instructor: Jason Galbraith
+ * Description: This is a program for creating and managing heap.
+ */
 #include <iostream>
 #include <fstream>
 #include <cstring>
+#include <limits>
 
 using namespace std;
 
@@ -12,15 +19,25 @@ int gen(int current);
 void draw(int index, int heap[], bool line[]);
 void insert(int list[], int index, int heap[]);
 void upheap(int current, int heap[]);
+void remove(int heap[], int heapTwo[]);
 
 int main() {
   // initialize variables
   char input[99];
+  int size;
   int method;
-  int list[101] = {0};
+  int list[100] = {0};
   int heap[201] = {0};
+  int heapTwo[201] = {0};
   
   // take input
+  cout << "\nEnter the amount of numbers: " << endl;
+  cout << "> ";
+  cin >> size;
+  if (size > 100) {
+    size = 100;
+  }
+  
   cout << "\nChoose input method[1/2]:" << endl;
   cout << "1. File" << endl;
   cout << "2. Manual" << endl;
@@ -36,7 +53,7 @@ int main() {
     cin.get(input, 99);
     infile.open(input);
     
-    for (int i = 0; i < 101; i++) {
+    for (int i = 0; i < size; i++) {
       infile >> list[i];
     }
   }
@@ -44,7 +61,7 @@ int main() {
     cout << "\nEnter the numbers:" << endl;
     cout << "> ";
 
-    for (int i = 0; i < 101; i++) {
+    for (int i = 0; i < size; i++) {
       cin >> list[i];
     }
   }
@@ -53,7 +70,7 @@ int main() {
   insert(list, 1, heap);
 
   // print out the list
-  cout << "\nList:" << endl;
+  cout << "\nList: ";
   int index = 0;
   while (list[index] != 0) {
     cout << list[index] << " ";
@@ -62,7 +79,7 @@ int main() {
   cout << endl;
 
   // print out the heap
-  cout << "\nHeap:" << endl;
+  cout << "\nHeap: ";
   int index2 = 1;
   while (heap[index2] != 0) {
     cout << heap[index2] << " ";
@@ -74,6 +91,17 @@ int main() {
   cout << "\nDiagram:" << endl;
   bool line[5] = {false};
   draw(1, heap, line);
+
+  // take out the root, repair, repeat
+  cout << "\nOutput:";
+  while (heap[1] != 0 || heapTwo[1] != 0) {
+    if (heap[1] == 0) {
+      remove(heapTwo, heap);
+    }
+    else {
+      remove(heap, heapTwo);
+    }
+  }
   
   return 0;
 }
@@ -90,6 +118,7 @@ int parent(int child) {
   return (child / 2);
 }
 
+// return the level a data is in
 int gen(int current) {
   int count = 1;
   while (current != 1) {
@@ -99,6 +128,7 @@ int gen(int current) {
   return count;
 }
 
+// insert numbers from a list into heap
 void insert(int list[], int index, int heap[]) {
   heap[index] = list[index-1];
   upheap(index, heap);
@@ -107,6 +137,7 @@ void insert(int list[], int index, int heap[]) {
   }
 }
 
+// upheap algorithm
 void upheap(int current, int heap[]) {
   if (current != 1) {
     if (heap[current] > heap[parent(current)]) {
@@ -118,6 +149,22 @@ void upheap(int current, int heap[]) {
   }
 }
 
+// take out the root from heap and transfer the rest into a new one
+void remove(int heap[], int heapTwo[]) {
+  int index = 1;
+  for (int i = 2; i < 201; i++) {
+    if (heap[i] != 0) {
+      heapTwo[index] = heap[i];
+      upheap(index, heapTwo);
+      index++;
+    }
+    heap[i] = 0;
+  }
+  cout << heap[1] << " ";
+  heap[1] = 0;
+}
+
+// print out the diagram
 void draw(int index, int heap[], bool line[]) {
   if (index == 1) {
     cout << heap[1] << endl;
